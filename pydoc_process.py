5dffcc1a0635e6592597a9fa3e20ab59477f32b4
David <91611885+FEDavid@users.noreply.github.com>
2023-02-21 16:17:16 +0000

    Added completed pydoc file

--- pydoc_process.py
+++ pydoc_process.py
@@ -1,39 +1,23 @@
-"""
-    Modules;
-    - Subprocess module to run bash within .py
-    Syntax is as follows
-    subprocess.run(["first_arg","second_arg","x_arg"]), for example subprocess.run(["python","-m","pydoc","-w","PyDoc_test"],text=True)
-
-    - pathlib is a module that we are utilizing to move files, in this case the syntax pathlib.Path("filename.ext").replace("newDir/filename.ext") allows the replacement of a file so we can keep documentation updated.
-
-    Extensions;
-    - autoDocstring - Python Docstring Generator - Put 3 quote marks (") to open documentation, then press tab to move through documentation and add details.
-"""
 import subprocess
-import pathlib
-import tkinter
+import os
+import shutil
 
-
-def pydoc_test():
-    """
-        Final function to output PyDoc generated documentation. If any modules have been added they will need to be manually added using the below syntax;
-        pathlib.Path("moduleName.html").replace("documentation/moduleName.html")
-
-        Please note, the modules name will also need to be added to the pydoc output.
-    """
+def pydoc_output():
     if __name__ == "__main__":
-        subprocess.run(["python","-m","pydoc","-w","PyDoc_test","subprocess","pathlib","tkinter"],text=True)
-        pathlib.Path("PyDoc_test.html").replace("documentation/PyDoc_test.html")
-        pathlib.Path("subprocess.html").replace("documentation/subprocess.html")
-        pathlib.Path("pathlib.html").replace("documentation/pathlib.html")
-        pathlib.Path("tkinter.html").replace("documentation/tkinter.html")
+        sourcepath=os.chdir("src")
+        sourcefiles = os.listdir(sourcepath)
+        for file in sourcefiles:
+            if file.endswith('.py'):
+                curr_file = file.strip(".py")
+                subprocess.run(["python3","-m","pydoc","-w","{0}".format(curr_file)],text=True)
 
-def test_func(num1,num2):
-    """_summary_
+def pydoc_move():
+    sourcepath=os.getcwd()
+    sourcefiles = os.listdir(sourcepath)
+    destinationpath = '../documentation/'  
+    for file in sourcefiles:
+        if file.endswith('.html'):
+            shutil.move(os.path.join(sourcepath,file), os.path.join(destinationpath,file))
 
-    Args:
-        num1 (_type_): _description_
-        num2 (_type_): _description_
-    """
-
-pydoc_test()
+pydoc_output()
+pydoc_move()
