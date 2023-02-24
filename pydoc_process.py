ee4bb7204b5e5c0cc8a101115783a6365ed547a0
David <91611885+FEDavid@users.noreply.github.com>
2023-02-24 16:54:46 +0000

    updated_pydoc

--- pydoc_process.py
+++ pydoc_process.py
@@ -2,30 +2,44 @@ import subprocess
 import os
 import shutil
 
+mainpath = os.getcwd()
+
 def pydoc_output():
     if __name__ == "__main__":
+        text = " START "
+        paths = []
+        print(f"{text:-^20}\n")
+        print("Finding files...")
         for root, dirs, files in os.walk(".", topdown=False):
             for file in files:
                 if file.endswith('.py'):
-                    if os.getcwd() != root:
-                        print("WORKING")
-                        print(os.path.relpath(root))
-                        os.chdir(os.path.relpath(root))
-                    print("")
-                    cwd = os.getcwd()
-                    print("CWD: - ",cwd,"\n","Root: ",root,sep="")
-                    # curr_file = file.strip(".py")
-                    # print(os.path.join(root, file))
-                    # subprocess.run(["python","-m","pydoc","-w","{0}".format(curr_file)],text=True)
+                    print("File found:",file)
+                    paths.append(root)
+        print("\nIn directories;")
+        paths = list(dict.fromkeys(paths))
+        print(paths)
+        print("\nOutputting files...")
+        for directory in paths:
+            os.chdir(mainpath)
+            dirname = os.path.relpath(directory)
+            os.chdir(dirname)
+            print("\nCWD: ",dirname,sep="")
+            subprocess.run(["python3","-m","pydoc","-w",".\\"])
 
 def pydoc_move():
     destinationpath = 'documentation/'  
+    os.chdir(mainpath)
+    print("Moving files...")
     for root, dirs, files in os.walk(".", topdown=False):
         for file in files:
             if file.endswith('.html'):
-                shutil.move(os.path.join(root,file), os.path.join(destinationpath,file))
-    text = " END "
-    print(f"\n{text:-^20}\n")
+                try:
+                    print("File moved:",file)
+                    shutil.move(os.path.join(root,file), os.path.join(destinationpath,file))
+                except:
+                    print("Something went wrong with the",file,"file.")
 
 pydoc_output()
-#pydoc_move()
+pydoc_move()
+text = " END "
+print(f"\n{text:-^20}")
