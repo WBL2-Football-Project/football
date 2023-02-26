21bda6379dac9fdc4c6e497f1dd532398adf0610
lukiies <lukiies@gmail.com>
2023-02-26 22:27:09 +0000

    next part of doc and src/model structure

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -16,7 +16,10 @@ from Play import *
 from DBPickleFile import *
 from UITkinterGUI import *
 from UIConsole import *
+from Widget import *
 from WidgetDefinition import *
+from Serialisable import *
 
 __all__ = ["Users","UnitTests","UIAbstractInterface","DBAbstractInterface","Teams","Schedule","StateMachine",
-	"SystemController","LoginStatus","Play","DBPickleFile","UITkinterGUI","UIConsole","WidgetDefinition","AccountRights","LayoutTypesEnum"]
+	"SystemController","LoginStatus","Play","DBPickleFile","UITkinterGUI","UIConsole","WidgetDefinition","AccountRights","LayoutTypesEnum",
+    "Serialisable","Widget"]
